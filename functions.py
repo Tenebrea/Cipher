209b9702c6f9a78e02ef78277656b7c1f326b63d
Tenebrea <andrejbulatov8673@gmail.com>
2025-09-19 21:29:38 +0300

    beta v0.2

--- functions.py
+++ functions.py
@@ -1,38 +1,40 @@
 import base64
 
-def caesars_cipher(line:str, step:int, decipher = False)->str:
+
+def caesars_cipher(line: str, step: int, decipher=False) -> str:
     keys_numbers = {
-    1: "А",  2: "Б",  3: "В",  4: "Г",  5: "Д",  6: "Е",
-    7: "Ё",  8: "Ж",  9: "З", 10: "И", 11: "Й", 12: "К",
-    13: "Л", 14: "М", 15: "Н", 16: "О", 17: "П", 18: "Р",
-    19: "С", 20: "Т", 21: "У", 22: "Ф", 23: "Х", 24: "Ц",
-    25: "Ч", 26: "Ш", 27: "Щ", 28: "Ъ", 29: "Ы", 30: "Ь",
-    31: "Э", 32: "Ю", 33: "Я"
-}
+        1: "А",  2: "Б",  3: "В",  4: "Г",  5: "Д",  6: "Е",
+        7: "Ё",  8: "Ж",  9: "З", 10: "И", 11: "Й", 12: "К",
+        13: "Л", 14: "М", 15: "Н", 16: "О", 17: "П", 18: "Р",
+        19: "С", 20: "Т", 21: "У", 22: "Ф", 23: "Х", 24: "Ц",
+        25: "Ч", 26: "Ш", 27: "Щ", 28: "Ъ", 29: "Ы", 30: "Ь",
+        31: "Э", 32: "Ю", 33: "Я"
+    }
     keys_letters = {
-    "А": 1,  "Б": 2,  "В": 3,  "Г": 4,  "Д": 5,  "Е": 6,
-    "Ё": 7,  "Ж": 8,  "З": 9,  "И": 10, "Й": 11, "К": 12,
-    "Л": 13, "М": 14, "Н": 15, "О": 16, "П": 17, "Р": 18,
-    "С": 19, "Т": 20, "У": 21, "Ф": 22, "Х": 23, "Ц": 24,
-    "Ч": 25, "Ш": 26, "Щ": 27, "Ъ": 28, "Ы": 29, "Ь": 30,
-    "Э": 31, "Ю": 32, "Я": 33
-}
+        "А": 1,  "Б": 2,  "В": 3,  "Г": 4,  "Д": 5,  "Е": 6,
+        "Ё": 7,  "Ж": 8,  "З": 9,  "И": 10, "Й": 11, "К": 12,
+        "Л": 13, "М": 14, "Н": 15, "О": 16, "П": 17, "Р": 18,
+        "С": 19, "Т": 20, "У": 21, "Ф": 22, "Х": 23, "Ц": 24,
+        "Ч": 25, "Ш": 26, "Щ": 27, "Ъ": 28, "Ы": 29, "Ь": 30,
+        "Э": 31, "Ю": 32, "Я": 33
+    }
     ans = ""
-    step%=33
-    if decipher==True:
-        step=-step
-    
+    step %= 33
+    if decipher == True:
+        step = -step
+
     for i in line:
         if i.upper() in keys_numbers.values():
             if i.isupper():
-                ans+=keys_numbers[keys_letters[i]+step]
+                ans += keys_numbers[keys_letters[i]+step]
             else:
-                ans+=keys_numbers[keys_letters[i.upper()]+step].lower()
+                ans += keys_numbers[keys_letters[i.upper()]+step].lower()
         else:
-            ans+=i
+            ans += i
     return ans
 
-def base64_encoder(line:str, encode = True)->str:    
+
+def base64_encoder(line: str, encode=True) -> str:
     if encode:
         try:
             return base64.b64encode(line.encode("ascii")).decode("ascii")
@@ -43,25 +45,26 @@ def base64_encoder(line:str, encode = True)->str:
             return base64.b64decode(line.encode("ascii")).decode("ascii")
         except:
             return "Это не Base64"
-        
 
 
-def verman_cipher(line:str, key:str, encode = True)->str:
+def verman_cipher(line: str, key: str, encode=True) -> str:
     if len(line) > len(key):
-        key+="o"*(len(line)-len(key))
+        key += "o"*(len(line)-len(key))
     return ''.join(chr(ord(p) ^ ord(k)) for p, k in zip(line, key))
 
-         
-def vigenere_cipher(line:str, key:str, encode = True)->str:    
-    key = key*(len(line)//len(key)) + key[:len(line)%len(key)]
+
+def vigenere_cipher(line: str, key: str, encode=True) -> str:
+    key = key*(len(line)//len(key)) + key[:len(line) % len(key)]
     text = []
     if encode:
         for i in range(len(line)):
             char = line[i]
             if char.isupper() and char != "Ё":
-                encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('А')) % 32 + ord('А'))
+                encrypted_char = chr(
+                    (ord(char) + ord(key[i]) - 2 * ord('А')) % 32 + ord('А'))
             elif char.islower() and char != "ё":
-                encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('а')) % 32 + ord('а'))
+                encrypted_char = chr(
+                    (ord(char) + ord(key[i]) - 2 * ord('а')) % 32 + ord('а'))
             else:
                 encrypted_char = char
             text.append(encrypted_char)
@@ -70,11 +73,12 @@ def vigenere_cipher(line:str, key:str, encode = True)->str:
         for i in range(len(line)):
             char = line[i]
             if char.isupper() and char != "Ё":
-                decrypted_char = chr((ord(char) - ord(key[i]) + 32) % 32 + ord('А'))
+                decrypted_char = chr(
+                    (ord(char) - ord(key[i]) + 32) % 32 + ord('А'))
             elif char.islower() and char != "ё":
-                decrypted_char = chr((ord(char) - ord(key[i]) + 32) % 32 + ord('а'))
+                decrypted_char = chr(
+                    (ord(char) - ord(key[i]) + 32) % 32 + ord('а'))
             else:
                 decrypted_char = char
             text.append(decrypted_char)
         return "".join(text)
-
--- main.py
+++ main.py
@@ -1,18 +1,50 @@
-import sys
-
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QMenu
+from PyQt6.QtWidgets import QApplication, QGridLayout, QLabel, QMainWindow, QMenu, QToolBar, QPushButton, QLineEdit, QWidget
+from functions import base64_encoder, verman_cipher, vigenere_cipher, caesars_cipher
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.show()
 
-        self.windowTitle("Шифратор")
+        self.create_toolbar()
 
-    
+        self.setWindowTitle("Encrypter")
+        self.setFixedSize(600, 300)
+
+    def remove_everything(self):
+        children = self.findChildren(QWidget)
+
+        for child in children:
+            child.deleteLater()
+
+        self.create_toolbar()
+
+    def vigenere_layout(self):
+        self.remove_everything()
+
+        self.input_text = QLineEdit(self)
+        self.input_text.move(50, 50)
+
+    def create_toolbar(self):
+        tool_bar = QToolBar("Main panel")
+        self.addToolBar(tool_bar)
+        tool_bar.setMovable(False)
+
+        action1 = QAction("Vigenere", self)
+        action1.triggered.connect(self.vigenere_layout)
+
+        action2 = QAction("Caesar", self)
+
+        action3 = QAction("BASE64", self)
+
+        action4 = QAction("Verman", self)
+
+        tool_bar.addAction(action1)
+        tool_bar.addAction(action2)
+        tool_bar.addAction(action3)
+        tool_bar.addAction(action4)
 
 
 app = QApplication([])
