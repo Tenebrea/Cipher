ae8f05805dec5414511572115de65adcb69e8400
Tenebrea <andrejbulatov8673@gmail.com>
2025-10-06 22:52:13 +0300

    Tweaks 1

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QAction, QActionGroup
 from PyQt6.QtWidgets import QApplication, QFileDialog, QStackedLayout, QVBoxLayout, QButtonGroup, QRadioButton, QGridLayout, QLabel, QMainWindow, QMenu, QToolBar, QPushButton, QLineEdit, QWidget
-from functions import base64_encoder, verman_cipher, vigenere_cipher, caesars_cipher
+from functions import base64_encoder, verman_cipher, vigenere_cipher, caesars_cipher, eng_caesars_cipher, eng_vigenere_cipher
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.encrypt = True
+        self.encrypt_state = True
+        self.getting_from_file = True
+        self.russian = False
         self.cipher = "vigenere"
         self.text = ""
         self.key = ""
@@ -137,8 +139,24 @@ class MainWindow(QMainWindow):
         self.addToolBar(self.tool_bar)
         self.tool_bar.setMovable(False)
 
+        menu = self.menuBar()
+
         self.action_group = QActionGroup(self)
         self.action_group.setExclusive(True)
+
+        self.getting_of_file = QActionGroup(self)
+        self.getting_of_file.setExclusive(True)
+
+        get_from_file = QAction("Из файла")
+        get_from_file.setStatusTip("Вводить текст и ключ из файла")
+        get_from_file.setCheckable(True)
+        get_from_file.setChecked(True)
+        get_from_file.triggered.connect(self.change_getting)
+
+        get_from_entries = QAction("Вводить")
+        get_from_entries.setStatusTip("Вводить текст в строку")
+        get_from_entries.setCheckable(True)
+        get_from_entries.triggered.connect(self.change_getting)
 
         action1 = QAction("Vigenere", self)
         action1.setCheckable(True)
@@ -157,23 +175,32 @@ class MainWindow(QMainWindow):
         action4.setCheckable(True)
         action4.triggered.connect(self.change_layout)
 
+        self.getting_of_file.addAction(get_from_entries)
+        self.getting_of_file.addAction(get_from_file)
+
         self.action_group.addAction(action1)
         self.action_group.addAction(action2)
-        self.action_group.addAction(action3)
         self.action_group.addAction(action4)
+        self.action_group.addAction(action3)
 
         self.tool_bar.addAction(action1)
         self.tool_bar.addAction(action2)
-        self.tool_bar.addAction(action3)
         self.tool_bar.addAction(action4)
+        self.tool_bar.addAction(action3)
+
+        file_menu = menu.addMenu("File")
+        file_menu.addAction(get_from_entries)
+        file_menu.addAction(get_from_file)
 
     def create_encryption(self):
         self.encrypt = QRadioButton("Зашифровать", self)
-        self.encrypt.move(10, 100)
+        self.encrypt.move(10, 150)
         self.encrypt.setChecked(True)
+        self.encrypt.clicked.connect(self.change_encrypt)
 
         self.decrypt = QRadioButton("Расшифровать", self)
-        self.decrypt.move(120, 100)
+        self.decrypt.move(120, 150)
+        self.decrypt.clicked.connect(self.change_encrypt)
 
         self.encrypt_radio_button = QButtonGroup(self)
 
@@ -182,15 +209,22 @@ class MainWindow(QMainWindow):
 
         self.get_result = QPushButton(self)
         self.get_result.setText("Получить")
-        self.get_result.move(10, 140)
+        self.get_result.move(10, 180)
         self.get_result.clicked.connect(self.encrypt_text)
 
     def change_encrypt(self):
         selected_button = self.encrypt_radio_button.checkedButton()
         if selected_button.text() == "Зашифровать":
-            self.encrypt = True
+            self.encrypt_state = True
         else:
-            self.encrypt = False
+            self.encrypt_state = False
+
+    def change_getting(self):
+        selected_action = self.getting_of_file.checkedAction()
+        if selected_action.text() == "Из файла":
+            self.getting_from_file = True
+        else:
+            self.getting_from_file = False
 
     def change_layout(self):
         self.vigenere_widget.hide()
@@ -248,18 +282,17 @@ class MainWindow(QMainWindow):
         )
         match self.cipher:
             case "vigenere":
-                self.text = vigenere_cipher(self.text, self.key, self.encrypt)
+                self.text = eng_vigenere_cipher(
+                    self.text, self.key, self.encrypt_state)
             case "caesar":
                 try:
                     self.key = int(self.input_key_caesar.text())
-                    self.text = caesars_cipher(
-                        self.text, self.key, self.encrypt)
+                    self.text = eng_caesars_cipher(
+                        self.text, self.key, self.encrypt_state)
                 except:
-                    self.key = ""
-                    self.text = ""
                     return
             case "base64":
-                self.text = base64_encoder(self.text, self.encrypt)
+                self.text = base64_encoder(self.text, self.encrypt_state)
             case "verman":
                 self.text = verman_cipher(self.text, self.key)
 
@@ -267,11 +300,7 @@ class MainWindow(QMainWindow):
             try:
                 with open(file_path, 'w', encoding='utf-8') as file:
                     file.write(self.text)
-                    self.text = ""
-                    self.key = ""
             except:
-                self.text = ""
-                self.key = ""
                 return
 
 
